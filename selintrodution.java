7b7b697152c2bd5a2268a143625de0bd70a99e8e
meenabogi456 <meenabogi7@gmail.com>
2023-04-06 11:05:17 +0530

    first commit

---
+++ selintrodution.java
@@ -0,0 +1,18 @@
+package introduction;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class selintrodution {
+
+	public static void main(String[] args) {
+	//invoking Browser
+		//chrome-chromeDriver ->Methods
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\intel\\Downloads\\chromedriver_win32 (4)\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		
+	
+	}
+
+}
